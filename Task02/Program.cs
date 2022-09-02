dced2bf09696c5fe16713225fb9b73464682143c
Dimonchik <sudjadred@live.com>
2022-09-02 21:08:04 +0200

    task02_corrections

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -5,11 +5,11 @@
 // a = -9 b = -3 -> max = -3
 
 Console.Clear();
-Console.WriteLine("Введите 2 числа. Мы определим, какое число больше.");
+Console.WriteLine("Введите 2 числа, чтобы узнать большее/ меньшее число");
 Console.Write("Введите первое число:  ");
 int numA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число:  ");
 int numB = Convert.ToInt32(Console.ReadLine());
-if (numA > numB) Console.WriteLine($"Первое число больше: {numA}");
-if (numB > numA) Console.WriteLine($"Второе число больше: {numB}");
+if (numA > numB) Console.WriteLine($"Большее число - {numA}; Меньшее число - {numB}");
+if (numB > numA) Console.WriteLine($"Большее число - {numB}; Меньшее число - {numA}");
 if (numA == numB) Console.WriteLine ("Числа равны");
